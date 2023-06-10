313b9676ab7019c85f543314f9189c0e5dd4a0e7
ComradeHibiscus <mark.vladimir.antonov@gmail.com>
2023-06-09 21:05:39 -0500

    PS4's pls work
    Hopefully the ps4 works in tankdrive class now and this is a working solution

--- src/main/java/frc/robot/commands/TankDrive.java
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -1,36 +1,52 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.RobotContainer;
+import frc.robot.subsystems.Drivebase;
 
-public class Tankdrive extends CommandBase {
 
-    public final DriveBase m_drivebase;
+public class TankDrive extends CommandBase{
+    
+    private final Drivebase m_drivebase;
+    private final PS4Controller driveController;
+    private SlewRateLimiter leftLimiter= new SlewRateLimiter(1);
+    private SlewRateLimiter rightLimiter = new SlewRateLimiter(1);
+    private double m_left;
+    private double m_right;
 
-    public Tankdrive (DriveBase drivebase) {   
+    public TankDrive (Drivebase drivebase, PS4Controller driveController) {   
         m_drivebase = drivebase;
+        this.driveController = driveController;
+
         addRequirements(m_drivebase);
     }
+
     @Override
     public void initialize () {
+        m_left = 0;
+        m_right = 0;
     }
     
     @Override
     public void execute() {
-        m_drivebase.drive(m_left.getAsDouble(), m_right.getAsDouble());
+        m_left = driveController.getRightY() + driveController.getRightX();
+        m_right = driveController.getRightY() - driveController.getRightX();
+        m_drivebase.drive(leftLimiter.calculate(m_left), rightLimiter.calculate(m_right));
     }
+
     @Override
-    public void end () {
+    public void end (boolean interrupted) {
         m_drivebase.drive(0.0, 0.0); 
     }
+
     @Override
     public boolean isFinished () {
         return false;
     }
     
     @Override
-    public void runsWhenDisabled(){
+    public boolean runsWhenDisabled(){
         return false; 
     }   
 }
--- src/main/java/frc/robot/subsystems/Drivebase.java
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -1,28 +1,52 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
-import edu.wpi.first.wpilibj.motorcontrol;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.util.sendable.Sendable;
+
 
 public class Drivebase extends SubsystemBase {
+    
+    private VictorSPX rightFrontVictor; 
+    private VictorSPX rightBackVictor;
+    private MotorControllerGroup rightMotorControl;
 
-    private VictorSPX rightFront;
-    private VictorSPX rightBack;
-    private VictorSPX leftBack;
-    private VictorSPX leftFront;
+    private VictorSPX leftBackVictor;
+    private VictorSPX leftFrontVictor;
+    private MotorControllerGroup leftMotorControl;
+    
+    private DifferentialDrive drive;
+    
+    public Drivebase() {   
+        rightFrontVictor = new VictorSPX(0);
+            addChild("rightFrontVictor", (Sendable) rightFrontVictor);
+            rightFrontVictor.setInverted(false);
 
-    /** Creates a new ExampleSubsystem. */
+        rightBackVictor = new VictorSPX(1);
+            addChild("rightBackVictor", (Sendable) rightBackVictor);
+            rightBackVictor.setInverted(false);
+        
+        leftBackVictor = new VictorSPX(2);
+            addChild("leftBackVictor", (Sendable) leftBackVictor);
+            leftBackVictor.setInverted(false);
+        
+        leftFrontVictor = new VictorSPX(3);
+            addChild("leftFrontVictor", (Sendable) leftFrontVictor);
+            leftFrontVictor.setInverted(false);
 
-    public Drivebase() {
+        rightMotorControl = new MotorControllerGroup((MotorController)rightBackVictor, (MotorController)rightFrontVictor);  
+        leftMotorControl = new MotorControllerGroup((MotorController)leftFrontVictor, (MotorController)leftBackVictor);
 
-       
+        drive = new DifferentialDrive(rightMotorControl, leftMotorControl);
+            addChild("drive", drive);
+            drive.setSafetyEnabled(true);
+            drive.setExpiration(0.1);
+            drive.setMaxOutput(1.0);
     }
-
+    
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
@@ -31,5 +55,9 @@ public class Drivebase extends SubsystemBase {
     @Override
     public void simulationPeriodic() {
         // This method will be called once per scheduler run during simulation
+    }
+
+    public void drive(double left, double right) {
+        drive.tankDrive(left, right);
     }
 }
